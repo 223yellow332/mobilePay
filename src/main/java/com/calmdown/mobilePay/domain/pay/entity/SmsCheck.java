62023a8a593d7b10687cae02b4459a4719fd7666
dannaa <83184270+danaKim-dokyung@users.noreply.github.com>
2023-11-10 03:34:15 +0900

    add SmsCheck Entity

---
+++ src/main/java/com/calmdown/mobilePay/domain/pay/entity/SmsCheck.java
@@ -0,0 +1,29 @@
+package com.calmdown.mobilePay.domain.pay.entity;
+
+import com.calmdown.mobilePay.domain.model.BaseTimeEntity;
+import jakarta.persistence.*;
+
+@Entity
+public class SmsCheck extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name="sms_check_id")
+    private long id;
+
+    //SMS(N) : 결제(1)
+    @JoinColumn(name="payment_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Payment payment;
+
+    //SMS 인증상태
+    @Column(name="result_code")
+    @Enumerated //[SMS_CHECK_READY, SMS_CHECK_SUCCESS,SMS_CHECK_FAILURE]
+    private SmsCheckStatus smsCheckStatus;
+
+    //SMS 인증번호
+    @Column(name="sms_auth_number")
+    private String smsAuthNumber;
+
+
+}
